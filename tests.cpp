c6c0e490822779b4b0dad29c01f9d911b91f2659
VictorNorman <vtn2@calvin.edu>
2023-07-11 11:20:29 -0400

    fix tests

--- tests.cpp
+++ tests.cpp
@@ -2,9 +2,16 @@
 #include "catch.hpp"
 #include "Student.h"
 
-TEST_CASE("student getters") {
-    Student st;
-    // SECTION("getMajor") {
-    //     REQUIRE(st.getMajor() == "Undecided");
-    // }
+TEST_CASE("Student class") {
+    SECTION("student getters") {
+        Student st;
+        // SECTION("getMajor") {
+        //     REQUIRE(st.getMajor() == "Undecided");
+        // }
+        // Add other SECTIONS here to test other getter methods.
+    }
+
+    // Add student setters SECTIONs here.
 }
+
+// Add TEST_CASE for Fraction class here. (Need to include Fraction.h when you do.)
